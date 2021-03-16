cd616234c518cfe425d8793acea11070a424b494
xackus <14938807+xackus@users.noreply.github.com>
2021-03-16 09:42:14 +0100

    split arg parsing from main logic

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,7 @@
 const std = @import("std");
 const mem = std.mem;
 const amqp = @import("zamqp");
-const zone2json = @import("zone2json.zig");
-const bytes = amqp.bytes_t.init;
+const server = @import("server.zig");
 
 var logOut: enum { syslog, stderr } = .syslog;
 
@@ -30,168 +29,6 @@ pub fn log(
             std.debug.print(level_prefix ++ scope_prefix ++ format ++ "\n", args);
         },
     }
-}
-
-fn logOrPanic(err: anyerror) void {
-    std.log.err("{}", .{err});
-    switch (err) {
-        error.OutOfMemory => {
-            // librabbitmq docs say it is not designed to handle OOM
-            std.debug.panic("out of memory", .{});
-        },
-        error.Unexpected => std.debug.panic("unexpected error", .{}),
-        else => {},
-    }
-}
-
-const ChannelState = struct {
-    unacked_count: u16 = 0,
-    last_delivery_tag: u64 = 0,
-};
-
-fn listen(alloc: *mem.Allocator, channel: amqp.Channel, state: *ChannelState, settings: ChannelSettings) !void {
-    channel.maybe_release_buffers();
-
-    var zero_timeval = std.c.timeval{ .tv_sec = 0, .tv_usec = 0 };
-
-    const block = state.unacked_count == 0;
-    var timeout = if (block) null else &zero_timeval;
-
-    var envelope = channel.connection.consume_message(timeout, 0) catch |err| switch (err) {
-        // a different frame needs to be read
-        error.UnexpectedState => {
-            const frame = try channel.connection.simple_wait_frame(null);
-            if (frame.frame_type == .METHOD) {
-                switch (frame.payload.method.id) {
-                    .CHANNEL_CLOSE => return error.ChannelClosed,
-                    .CONNECTION_CLOSE => return error.ConnectionClosed,
-                    else => return error.UnexpectedMethod,
-                }
-            } else return error.UnexpectedFrame;
-        },
-        error.Timeout => {
-            try channel.basic_ack(state.last_delivery_tag, true);
-            state.unacked_count = 0;
-            std.log.debug("no messages, acked partial batch (delivery tag: {d})", .{state.last_delivery_tag});
-            return;
-        },
-        else => return err,
-    };
-    defer envelope.destroy();
-
-    const reply_to = envelope.message.properties.get(.reply_to) orelse {
-        try channel.basic_reject(envelope.delivery_tag, false);
-        return;
-    };
-
-    state.unacked_count += 1;
-    state.last_delivery_tag = envelope.delivery_tag;
-
-    var json = try std.ArrayList(u8).initCapacity(alloc, 4096);
-    defer json.deinit();
-
-    const valid_content_type = if (envelope.message.properties.get(.content_type)) |content_type|
-        mem.eql(u8, content_type.slice().?, "text/dns")
-    else
-        false;
-
-    if (valid_content_type) {
-        try zone2json.convertApi(envelope.message.body.slice().?, &json);
-    } else {
-        json.appendSliceAssumeCapacity(
-            \\{"error":"invalid content type"}
-        );
-    }
-
-    var properties = amqp.BasicProperties.init(.{
-        .content_type = bytes("application/json"),
-    });
-    if (envelope.message.properties.get(.correlation_id)) |id| {
-        properties.set(.correlation_id, id);
-    }
-
-    try channel.basic_publish(
-        bytes(""),
-        reply_to,
-        bytes(json.items),
-        properties,
-        .{},
-    );
-
-    if (state.unacked_count == settings.max_batch) {
-        try channel.basic_ack(state.last_delivery_tag, true);
-        state.unacked_count = 0;
-        std.log.debug("acked full batch (delivery tag: {d})", .{state.last_delivery_tag});
-    }
-}
-
-const ChannelSettings = struct {
-    queue: []const u8,
-    prefetch_count: u16,
-    max_batch: u16,
-};
-
-fn setupChannel(channel: amqp.Channel, settings: ChannelSettings) !void {
-    std.log.info("opening channel {d}", .{channel.number});
-
-    _ = try channel.open();
-    errdefer channel.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
-
-    std.log.info("consuming from queue {s}", .{settings.queue});
-
-    try channel.basic_qos(0, settings.prefetch_count, false);
-    _ = try channel.basic_consume(bytes(settings.queue), .{});
-
-    std.log.info("channel set up", .{});
-}
-
-const ConnectionSettings = struct {
-    port: c_int,
-    host: [*:0]const u8,
-    vhost: [*:0]const u8,
-    auth: amqp.Connection.SaslAuth,
-    /// null disables TLS
-    ca_cert: ?[*:0]const u8,
-    heartbeat: c_int,
-};
-
-fn setupConnection(conn: amqp.Connection, settings: ConnectionSettings) !void {
-    std.log.info("connecting to {s}:{d}", .{ settings.host, settings.port });
-
-    if (settings.ca_cert) |ca_cert| {
-        const sock = try amqp.SslSocket.new(conn);
-        sock.set_verify_peer(true);
-        sock.set_verify_hostname(true);
-        try sock.set_cacert(ca_cert);
-        try sock.open(settings.host, settings.port, null);
-    } else {
-        const sock = try amqp.TcpSocket.new(conn);
-        try sock.open(settings.host, settings.port, null);
-    }
-
-    errdefer conn.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
-
-    std.log.info("logging into vhost {s} as {s}", .{ settings.vhost, settings.auth.plain.username });
-
-    try conn.login(settings.vhost, settings.auth, .{ .heartbeat = settings.heartbeat });
-
-    std.log.info("connection set up", .{});
-}
-
-fn exponentialBackoff(backoff: *i64, random: *std.rand.Random) void {
-    if (backoff.* == 0) {
-        // first time try again without waiting
-        backoff.* = 1000;
-        return;
-    }
-
-    const start = std.time.milliTimestamp();
-    var to_sleep: i64 = backoff.* + random.uintLessThanBiased(u32, 1000);
-    while (to_sleep > 1) {
-        std.time.sleep(@intCast(u64, to_sleep * std.time.ns_per_ms));
-        to_sleep -= std.time.milliTimestamp() - start;
-    }
-    if (backoff.* < 32_000) backoff.* *= 2;
 }
 
 fn fatal(comptime fmt: []const u8, args: anytype) noreturn {
@@ -354,7 +191,7 @@ pub fn main() !void {
     var ca_cert: ?[*:0]const u8 = null;
     var heartbeat: c_int = 60;
 
-    var chanSettings = ChannelSettings{
+    var chan_settings = server.ChannelSettings{
         .queue = "zone2json",
         .prefetch_count = 10,
         .max_batch = 10,
@@ -397,11 +234,11 @@ pub fn main() !void {
         } else if (args.get("heartbeat", c_int)) |val| {
             heartbeat = val;
         } else if (args.get("queue", str)) |val| {
-            chanSettings.queue = val;
+            chan_settings.queue = val;
         } else if (args.get("prefetch-count", u16)) |val| {
-            chanSettings.prefetch_count = val;
+            chan_settings.prefetch_count = val;
         } else if (args.get("batch", u16)) |val| {
-            chanSettings.max_batch = if (val == 0) 1 else val;
+            chan_settings.max_batch = if (val == 0) 1 else val;
         } else if (args.get("no-tls", void)) {
             tls = false;
         } else if (args.get("help", void)) {
@@ -417,49 +254,14 @@ pub fn main() !void {
 
     if (port == null) port = if (tls) 5671 else 5672;
 
-    var conn = try amqp.Connection.new();
-    defer conn.destroy() catch |err| logOrPanic(err);
-
-    var backoff_ms: i64 = 0;
-    var rng = std.rand.DefaultPrng.init(std.crypto.random.int(u64));
-
-    connection: while (true) {
-        setupConnection(conn, .{
-            .port = port.?,
-            .host = host,
-            .vhost = vhost,
-            .auth = .{ .plain = .{ .username = user, .password = password } },
-            .ca_cert = ca_cert,
-            .heartbeat = heartbeat,
-        }) catch |err| {
-            logOrPanic(err);
-            exponentialBackoff(&backoff_ms, &rng.random);
-            continue :connection;
-        };
-        defer conn.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
-
-        channel: while (true) {
-            const channel = conn.channel(1);
-            setupChannel(channel, chanSettings) catch |err| {
-                logOrPanic(err);
-                exponentialBackoff(&backoff_ms, &rng.random);
-                continue :connection;
-            };
-            defer channel.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
-
-            var state = ChannelState{};
-
-            while (true) {
-                listen(alloc, channel, &state, chanSettings) catch |err| {
-                    logOrPanic(err);
-                    exponentialBackoff(&backoff_ms, &rng.random);
-                    if (err == error.ChannelClosed) continue :channel;
-                    continue :connection;
-                };
-                backoff_ms = 0;
-            }
-        }
-    }
+    try server.run(alloc, .{
+        .port = port.?,
+        .host = host,
+        .vhost = vhost,
+        .auth = .{ .plain = .{ .username = user, .password = password } },
+        .ca_cert = ca_cert,
+        .heartbeat = heartbeat,
+    }, chan_settings);
 }
 
 fn help() !void {
---
+++ src/server.zig
@@ -0,0 +1,205 @@
+const std = @import("std");
+const amqp = @import("zamqp");
+const bytes = amqp.bytes_t.init;
+const zone2json = @import("zone2json.zig");
+
+fn logOrPanic(err: anyerror) void {
+    std.log.err("{}", .{err});
+    switch (err) {
+        error.OutOfMemory => {
+            // librabbitmq docs say it is not designed to handle OOM
+            std.debug.panic("out of memory", .{});
+        },
+        error.Unexpected => std.debug.panic("unexpected error", .{}),
+        else => {},
+    }
+}
+
+const ChannelState = struct {
+    unacked_count: u16 = 0,
+    last_delivery_tag: u64 = 0,
+};
+
+fn listen(alloc: *std.mem.Allocator, channel: amqp.Channel, state: *ChannelState, settings: ChannelSettings) !void {
+    channel.maybe_release_buffers();
+
+    var zero_timeval = std.c.timeval{ .tv_sec = 0, .tv_usec = 0 };
+
+    const block = state.unacked_count == 0;
+    var timeout = if (block) null else &zero_timeval;
+
+    var envelope = channel.connection.consume_message(timeout, 0) catch |err| switch (err) {
+        // a different frame needs to be read
+        error.UnexpectedState => {
+            const frame = try channel.connection.simple_wait_frame(null);
+            if (frame.frame_type == .METHOD) {
+                switch (frame.payload.method.id) {
+                    .CHANNEL_CLOSE => return error.ChannelClosed,
+                    .CONNECTION_CLOSE => return error.ConnectionClosed,
+                    else => return error.UnexpectedMethod,
+                }
+            } else return error.UnexpectedFrame;
+        },
+        error.Timeout => {
+            try channel.basic_ack(state.last_delivery_tag, true);
+            state.unacked_count = 0;
+            std.log.debug("no messages, acked partial batch (delivery tag: {d})", .{state.last_delivery_tag});
+            return;
+        },
+        else => return err,
+    };
+    defer envelope.destroy();
+
+    const reply_to = envelope.message.properties.get(.reply_to) orelse {
+        try channel.basic_reject(envelope.delivery_tag, false);
+        return;
+    };
+
+    state.unacked_count += 1;
+    state.last_delivery_tag = envelope.delivery_tag;
+
+    var json = try std.ArrayList(u8).initCapacity(alloc, 4096);
+    defer json.deinit();
+
+    const valid_content_type = if (envelope.message.properties.get(.content_type)) |content_type|
+        std.mem.eql(u8, content_type.slice().?, "text/dns")
+    else
+        false;
+
+    if (valid_content_type) {
+        try zone2json.convertApi(envelope.message.body.slice().?, &json);
+    } else {
+        json.appendSliceAssumeCapacity(
+            \\{"error":"invalid content type"}
+        );
+    }
+
+    var properties = amqp.BasicProperties.init(.{
+        .content_type = bytes("application/json"),
+    });
+    if (envelope.message.properties.get(.correlation_id)) |id| {
+        properties.set(.correlation_id, id);
+    }
+
+    try channel.basic_publish(
+        bytes(""),
+        reply_to,
+        bytes(json.items),
+        properties,
+        .{},
+    );
+
+    if (state.unacked_count == settings.max_batch) {
+        try channel.basic_ack(state.last_delivery_tag, true);
+        state.unacked_count = 0;
+        std.log.debug("acked full batch (delivery tag: {d})", .{state.last_delivery_tag});
+    }
+}
+
+pub const ChannelSettings = struct {
+    queue: []const u8,
+    prefetch_count: u16,
+    max_batch: u16,
+};
+
+fn setupChannel(channel: amqp.Channel, settings: ChannelSettings) !void {
+    std.log.info("opening channel {d}", .{channel.number});
+
+    _ = try channel.open();
+    errdefer channel.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
+
+    std.log.info("consuming from queue {s}", .{settings.queue});
+
+    try channel.basic_qos(0, settings.prefetch_count, false);
+    _ = try channel.basic_consume(bytes(settings.queue), .{});
+
+    std.log.info("channel set up", .{});
+}
+
+pub const ConnectionSettings = struct {
+    port: c_int,
+    host: [*:0]const u8,
+    vhost: [*:0]const u8,
+    auth: amqp.Connection.SaslAuth,
+    /// null disables TLS
+    ca_cert: ?[*:0]const u8,
+    heartbeat: c_int,
+};
+
+fn setupConnection(conn: amqp.Connection, settings: ConnectionSettings) !void {
+    std.log.info("connecting to {s}:{d}", .{ settings.host, settings.port });
+
+    if (settings.ca_cert) |ca_cert| {
+        const sock = try amqp.SslSocket.new(conn);
+        sock.set_verify_peer(true);
+        sock.set_verify_hostname(true);
+        try sock.set_cacert(ca_cert);
+        try sock.open(settings.host, settings.port, null);
+    } else {
+        const sock = try amqp.TcpSocket.new(conn);
+        try sock.open(settings.host, settings.port, null);
+    }
+
+    errdefer conn.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
+
+    std.log.info("logging into vhost {s} as {s}", .{ settings.vhost, settings.auth.plain.username });
+
+    try conn.login(settings.vhost, settings.auth, .{ .heartbeat = settings.heartbeat });
+
+    std.log.info("connection set up", .{});
+}
+
+fn exponentialBackoff(backoff: *i64, random: *std.rand.Random) void {
+    if (backoff.* == 0) {
+        // first time try again without waiting
+        backoff.* = 1000;
+        return;
+    }
+
+    const start = std.time.milliTimestamp();
+    var to_sleep: i64 = backoff.* + random.uintLessThanBiased(u32, 1000);
+    while (to_sleep > 1) {
+        std.time.sleep(@intCast(u64, to_sleep * std.time.ns_per_ms));
+        to_sleep -= std.time.milliTimestamp() - start;
+    }
+    if (backoff.* < 32_000) backoff.* *= 2;
+}
+
+pub fn run(alloc: *std.mem.Allocator, con_settings: ConnectionSettings, chan_settings: ChannelSettings) !void {
+    var conn = try amqp.Connection.new();
+    defer conn.destroy() catch |err| logOrPanic(err);
+
+    var backoff_ms: i64 = 0;
+    var rng = std.rand.DefaultPrng.init(std.crypto.random.int(u64));
+
+    connection: while (true) {
+        setupConnection(conn, con_settings) catch |err| {
+            logOrPanic(err);
+            exponentialBackoff(&backoff_ms, &rng.random);
+            continue :connection;
+        };
+        defer conn.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
+
+        channel: while (true) {
+            const channel = conn.channel(1);
+            setupChannel(channel, chan_settings) catch |err| {
+                logOrPanic(err);
+                exponentialBackoff(&backoff_ms, &rng.random);
+                continue :connection;
+            };
+            defer channel.close(.REPLY_SUCCESS) catch |err| logOrPanic(err);
+
+            var state = ChannelState{};
+
+            while (true) {
+                listen(alloc, channel, &state, chan_settings) catch |err| {
+                    logOrPanic(err);
+                    exponentialBackoff(&backoff_ms, &rng.random);
+                    if (err == error.ChannelClosed) continue :channel;
+                    continue :connection;
+                };
+                backoff_ms = 0;
+            }
+        }
+    }
+}
